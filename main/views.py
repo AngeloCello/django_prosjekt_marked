fd11faf2ca2f109f44849e2012cbe259e13d55de
angelocello <ajc155@live.com>
2022-06-03 15:16:42 +0200

    Added HTML

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def homepage(request):
+    return render(request, template_name='main/home.html')
+
+def itemspage(request):
+    items = [
+        {
+            'name': 'Phone',
+            'price': '500'
+        },
+        {
+            'name': 'Laptop',
+            'price': '1000'
+        }
+    ]
+
+    return render(request, template_name='main/items.html', context={'items': items})
