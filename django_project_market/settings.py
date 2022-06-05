c0fd0b50a6fe6c83c647f01d459d18029448bb6b
angelocello <ajc155@live.com>
2022-06-05 13:24:27 +0200

    test commit

--- django_project_market/settings.py
+++ django_project_market/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+#example commit test
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
