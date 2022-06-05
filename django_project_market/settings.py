ec9700af86a7991b0819f9564e8f24a1e8ec58d8
angelocello <ajc155@live.com>
2022-06-05 11:30:44 +0200

    first commit

--- django_project_market/settings.py
+++ django_project_market/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_3yw%i%cu!k8seb2*#jukjp!!uy&$@^bxy#o7ifz*s5v4e33qn'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['angelo-django-project1.herokuapp.com', '127.0.0.1']
 
